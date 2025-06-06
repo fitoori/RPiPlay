45191aa32fe1e97a926a2c2f57f2c9c119a59366
fitoori <sbajaj@me.com>
2025-06-06 15:48:03 -0400

    Update rpiplay.cpp
    enhanced

--- rpiplay.cpp
+++ rpiplay.cpp
@@ -1,7 +1,7 @@
 /**
  * RPiPlay - An open-source AirPlay mirroring server for Raspberry Pi
  * Copyright (C) 2019 Florian Draschbacher
- *
+ * Modified with the help of AI by github.com/fitoori
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -19,11 +19,13 @@
 
 #include <stddef.h>
 #include <cstring>
-#include <signal.h>
+#include <csignal>
 #include <unistd.h>
 #include <string>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <array>
 
 #include <sys/socket.h>
 #include <ifaddrs.h>
@@ -72,7 +74,8 @@ typedef struct audio_renderer_list_entry_s {
     audio_init_func_t init_func;
 } audio_renderer_list_entry_t;
 
-static bool running = false;
+// Using sig_atomic_t to comply with async‑signal‑safe requirements
+static volatile sig_atomic_t running = 0;
 static dnssd_t *dnssd = NULL;
 static raop_t *raop = NULL;
 static video_init_func_t video_init_func = NULL;
@@ -105,6 +108,10 @@ static const audio_renderer_list_entry_t audio_renderers[] = {
 #endif
 };
 
+// Ensure at least one renderer is compiled in; gives clear build‑time error otherwise
+static_assert(sizeof(video_renderers) / sizeof(video_renderers[0]) > 0, "At least one video renderer must be enabled");
+static_assert(sizeof(audio_renderers) / sizeof(audio_renderers[0]) > 0, "At least one audio renderer must be enabled");
+
 static void signal_handler(int sig) {
     switch (sig) {
         case SIGINT:
@@ -115,7 +122,7 @@ static void signal_handler(int sig) {
 }
 
 static void init_signals(void) {
-    struct sigaction sigact;
+    struct sigaction sigact{};
 
     sigact.sa_handler = signal_handler;
     sigemptyset(&sigact.sa_mask);
@@ -124,55 +131,68 @@ static void init_signals(void) {
     sigaction(SIGTERM, &sigact, NULL);
 }
 
-static int parse_hw_addr(std::string str, std::vector<char> &hw_addr) {
-    for (int i = 0; i < str.length(); i += 3) {
-        hw_addr.push_back((char) stol(str.substr(i), NULL, 16));
+// Accepts both colon‑delimited and raw 12‑hex‑digit MAC strings
+static int parse_hw_addr(const std::string &str, std::vector<char> &hw_addr) {
+    hw_addr.clear();
+    size_t i = 0;
+    while (i < str.length()) {
+        if (str[i] == ':' || str[i] == '-' || str[i] == ' ') { // skip delimiters
+            ++i;
+            continue;
+        }
+        if (i + 1 >= str.length()) {
+            return -1; // malformed
+        }
+        char byte_str[3] = {str[i], str[i + 1], '\0'};
+        hw_addr.push_back(static_cast<char>(strtol(byte_str, NULL, 16)));
+        i += 2;
     }
-    return 0;
+    return hw_addr.size() == 6 ? 0 : -1;
 }
 
 static std::string find_mac () {
 /*  finds the MAC address of the first active network interface *
  *  in a Linux, *BSD or macOS system.                           */
-    std::string mac_address = "";
-    struct ifaddrs *ifap, *ifaptr;
-    int non_null_octets = 0;
-    unsigned char octet[6], *ptr;
-    if (getifaddrs(&ifap) == 0) {
-        for(ifaptr = ifap; ifaptr != NULL; ifaptr = ifaptr->ifa_next) {
-            if(ifaptr->ifa_addr == NULL) continue;
+    std::string mac_address;
+    struct ifaddrs *ifap = nullptr, *ifaptr = nullptr;
+    if (getifaddrs(&ifap) != 0) {
+        return mac_address; // empty
+    }
+
+    for(ifaptr = ifap; ifaptr != NULL; ifaptr = ifaptr->ifa_next) {
+        if(ifaptr->ifa_addr == NULL) continue;
+        int non_null_octets = 0; // reset for every interface
+        unsigned char octet[6] = {0};
 #ifdef __linux__
-            if (ifaptr->ifa_addr->sa_family != AF_PACKET) continue;
-            struct sockaddr_ll *s = (struct sockaddr_ll*) ifaptr->ifa_addr;
-            for (int i = 0; i < 6; i++) {
-                if ((octet[i] = s->sll_addr[i]) != 0) non_null_octets++;
-            }
+        if (ifaptr->ifa_addr->sa_family != AF_PACKET) continue;
+        struct sockaddr_ll *s = (struct sockaddr_ll*) ifaptr->ifa_addr;
+        for (int i = 0; i < 6; i++) {
+            if ((octet[i] = s->sll_addr[i]) != 0) non_null_octets++;
+        }
 #else    /* macOS and *BSD */
-            if (ifaptr->ifa_addr->sa_family != AF_LINK) continue;
-            ptr = (unsigned char *) LLADDR((struct sockaddr_dl *) ifaptr->ifa_addr);
-            for (int i= 0; i < 6 ; i++) {
-                if ((octet[i] = *ptr) != 0) non_null_octets++;
-                ptr++;
-            }
+        if (ifaptr->ifa_addr->sa_family != AF_LINK) continue;
+        unsigned char *ptr = (unsigned char *) LLADDR((struct sockaddr_dl *) ifaptr->ifa_addr);
+        for (int i = 0; i < 6 ; i++) {
+            if ((octet[i] = ptr[i]) != 0) non_null_octets++;
+        }
 #endif
-            if (non_null_octets) {
-                mac_address.erase();
-                char str[3];
-                for (int i = 0; i < 6 ; i++) {
-                    sprintf(str,"%02x", octet[i]);
-                    mac_address = mac_address + str;
-                    if (i < 5) mac_address = mac_address + ":";
-                }
-                break;
+        if (non_null_octets) {
+            std::ostringstream oss;
+            for (int i = 0; i < 6 ; i++) {
+                oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(octet[i]);
+                if (i < 5) oss << ":";
             }
+            mac_address = oss.str();
+            break;
         }
     }
+
     freeifaddrs(ifap);
     return mac_address;
 }
 
 static video_init_func_t find_video_init_func(const char *name) {
-    for (int i = 0; i < sizeof(video_renderers)/sizeof(video_renderers[0]); i++) {
+    for (size_t i = 0; i < sizeof(video_renderers)/sizeof(video_renderers[0]); i++) {
         if (!strcmp(name, video_renderers[i].name)) {
             return video_renderers[i].init_func;
         }
@@ -181,7 +201,7 @@ static video_init_func_t find_video_init_func(const char *name) {
 }
 
 static audio_init_func_t find_audio_init_func(const char *name) {
-    for (int i = 0; i < sizeof(audio_renderers)/sizeof(audio_renderers[0]); i++) {
+    for (size_t i = 0; i < sizeof(audio_renderers)/sizeof(audio_renderers[0]); i++) {
         if (!strcmp(name, audio_renderers[i].name)) {
             return audio_renderers[i].init_func;
         }
@@ -200,11 +220,11 @@ void print_info(char *name) {
     printf("-l                    Enable low-latency mode (disables render clock)\n");
     printf("-a (hdmi|analog|off)  Set audio output device\n");
     printf("-vr renderer          Set video renderer to use. Available renderers:\n");
-    for (int i = 0; i < sizeof(video_renderers)/sizeof(video_renderers[0]); i++) {
+    for (size_t i = 0; i < sizeof(video_renderers)/sizeof(video_renderers[0]); i++) {
         printf("    %s: %s%s\n", video_renderers[i].name, video_renderers[i].description, i == 0 ? " [Default]" : "");
     }
     printf("-ar renderer          Set audio renderer to use. Available renderers:\n");
-    for (int i = 0; i < sizeof(audio_renderers)/sizeof(audio_renderers[0]); i++) {
+    for (size_t i = 0; i < sizeof(audio_renderers)/sizeof(audio_renderers[0]); i++) {
         printf("    %s: %s%s\n", audio_renderers[i].name, audio_renderers[i].description, i == 0 ? " [Default]" : "");
     }
     printf("-d                    Enable debug logging\n");
@@ -213,22 +233,22 @@ void print_info(char *name) {
 
 int main(int argc, char *argv[]) {
     init_signals();
-    
+
     std::string server_name = DEFAULT_NAME;
     std::vector<char> server_hw_addr = DEFAULT_HW_ADDRESS;
     bool debug_log = DEFAULT_DEBUG_LOG;
 
-    video_renderer_config_t video_config;
+    video_renderer_config_t video_config{};
     video_config.background_mode = DEFAULT_BACKGROUND_MODE;
     video_config.low_latency = DEFAULT_LOW_LATENCY;
     video_config.rotation = DEFAULT_ROTATE;
     video_config.flip = DEFAULT_FLIP;
-    
-    audio_renderer_config_t audio_config;
+
+    audio_renderer_config_t audio_config{};
     audio_config.device = DEFAULT_AUDIO_DEVICE;
     audio_config.low_latency = DEFAULT_LOW_LATENCY;
-    
-    // Default to the best available renderer
+
+    // Default to the first listed renderer (compile-time asserts guarantee at least one)
     video_init_func = video_renderers[0].init_func;
     audio_init_func = audio_renderers[0].init_func;
 
@@ -259,6 +279,7 @@ int main(int argc, char *argv[]) {
             video_config.low_latency = !video_config.low_latency;
             audio_config.low_latency = !audio_config.low_latency;
         } else if (arg == "-r") {
+            if (i == argc - 1) continue; // guard against missing value
             video_config.rotation = atoi(argv[++i]);
         } else if (arg == "-f") {
             if (i == argc - 1) continue;
@@ -280,168 +301,4 @@ int main(int argc, char *argv[]) {
                 exit(1);
             }
         } else if (arg == "-ar") {
-            if (i == argc - 1) {
-                fprintf(stderr, "Error: You must supply the name of an audio renderer after the -ar argument.\n");
-                exit(1);
-            }
-            audio_init_func = find_audio_init_func(argv[++i]);
-            if (!audio_init_func) {
-                fprintf(stderr, "Error: Unable to locate audio renderer \"%s\".\n", argv[i]);
-                exit(1);
-            }
-        } else if (arg == "-h" || arg == "-v") {
-            print_info(argv[0]);
-            exit(0);
-        }
-    }
-
-    std::string mac_address = find_mac();
-    if (!mac_address.empty()) {
-        server_hw_addr.clear();
-        parse_hw_addr(mac_address, server_hw_addr);
-    }
-
-    if (start_server(server_hw_addr, server_name, debug_log, &video_config, &audio_config) != 0) {
-        return 1;
-    }
-
-    running = true;
-    while (running) {
-        sleep(1);
-    }
-
-    LOGI("Stopping...");
-    stop_server();
-}
-
-// Server callbacks
-extern "C" void conn_init(void *cls) {
-    if (video_renderer) video_renderer->funcs->update_background(video_renderer, 1);
-}
-
-extern "C" void conn_destroy(void *cls) {
-    if (video_renderer) video_renderer->funcs->update_background(video_renderer, -1);
-}
-
-extern "C" void audio_process(void *cls, raop_ntp_t *ntp, aac_decode_struct *data) {
-    if (audio_renderer != NULL) {
-        audio_renderer->funcs->render_buffer(audio_renderer, ntp, data->data, data->data_len, data->pts);
-    }
-}
-
-extern "C" void video_process(void *cls, raop_ntp_t *ntp, h264_decode_struct *data) {
-    if (video_renderer != NULL) {
-        video_renderer->funcs->render_buffer(video_renderer, ntp, data->data, data->data_len, data->pts, data->frame_type);
-    }
-}
-
-extern "C" void audio_flush(void *cls) {
-    if (audio_renderer) audio_renderer->funcs->flush(audio_renderer);
-}
-
-extern "C" void video_flush(void *cls) {
-    if (video_renderer) video_renderer->funcs->flush(video_renderer);
-}
-
-extern "C" void audio_set_volume(void *cls, float volume) {
-    if (audio_renderer != NULL) {
-        audio_renderer->funcs->set_volume(audio_renderer, volume);
-    }
-}
-
-extern "C" void log_callback(void *cls, int level, const char *msg) {
-    switch (level) {
-        case LOGGER_DEBUG: {
-            LOGD("%s", msg);
-            break;
-        }
-        case LOGGER_WARNING: {
-            LOGW("%s", msg);
-            break;
-        }
-        case LOGGER_INFO: {
-            LOGI("%s", msg);
-            break;
-        }
-        case LOGGER_ERR: {
-            LOGE("%s", msg);
-            break;
-        }
-        default:
-            break;
-    }
-
-}
-
-int start_server(std::vector<char> hw_addr, std::string name, bool debug_log,
-                 video_renderer_config_t const *video_config, audio_renderer_config_t const *audio_config) {
-    raop_callbacks_t raop_cbs;
-    memset(&raop_cbs, 0, sizeof(raop_cbs));
-    raop_cbs.conn_init = conn_init;
-    raop_cbs.conn_destroy = conn_destroy;
-    raop_cbs.audio_process = audio_process;
-    raop_cbs.video_process = video_process;
-    raop_cbs.audio_flush = audio_flush;
-    raop_cbs.video_flush = video_flush;
-    raop_cbs.audio_set_volume = audio_set_volume;
-
-    raop = raop_init(10, &raop_cbs);
-    if (raop == NULL) {
-        LOGE("Error initializing raop!");
-        return -1;
-    }
-
-    raop_set_log_callback(raop, log_callback, NULL);
-    raop_set_log_level(raop, debug_log ? RAOP_LOG_DEBUG : LOGGER_INFO);
-
-    render_logger = logger_init();
-    logger_set_callback(render_logger, log_callback, NULL);
-    logger_set_level(render_logger, debug_log ? LOGGER_DEBUG : LOGGER_INFO);
-
-    if (video_config->low_latency) logger_log(render_logger, LOGGER_INFO, "Using low-latency mode");
-
-    if ((video_renderer = video_init_func(render_logger, video_config)) == NULL) {
-        LOGE("Could not init video renderer");
-        return -1;
-    }
-
-    if (audio_config->device == AUDIO_DEVICE_NONE) {
-        LOGI("Audio disabled");
-    } else if ((audio_renderer = audio_init_func(render_logger, video_renderer, audio_config)) ==
-               NULL) {
-        LOGE("Could not init audio renderer");
-        return -1;
-    }
-
-    if (video_renderer) video_renderer->funcs->start(video_renderer);
-    if (audio_renderer) audio_renderer->funcs->start(audio_renderer);
-
-    unsigned short port = 0;
-    raop_start(raop, &port);
-    raop_set_port(raop, port);
-
-    int error;
-    dnssd = dnssd_init(name.c_str(), strlen(name.c_str()), hw_addr.data(), hw_addr.size(), &error);
-    if (error) {
-        LOGE("Could not initialize dnssd library!");
-        return -2;
-    }
-
-    raop_set_dnssd(raop, dnssd);
-
-    dnssd_register_raop(dnssd, port);
-    dnssd_register_airplay(dnssd, port + 1);
-
-    return 0;
-}
-
-int stop_server() {
-    raop_destroy(raop);
-    dnssd_unregister_raop(dnssd);
-    dnssd_unregister_airplay(dnssd);
-    // If we don't destroy these two in the correct order, we get a deadlock from the ilclient library
-    if (audio_renderer) audio_renderer->funcs->destroy(audio_renderer);
-    if (video_renderer) video_renderer->funcs->destroy(video_renderer);
-    logger_destroy(render_logger);
-    return 0;
-}
+            if (
